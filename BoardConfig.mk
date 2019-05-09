80d1d2955d54ee8cd50a59e2a218f60bdf9721ac
Swetha Chikkaboraiah <schikk@codeaurora.org>
2019-05-09 11:51:22 +0530

    bullhead: Make swiotlb 4MB from command line parameter.
    This parameter "swiotlb=2048" sets the size of software
    IO TLB to defualt value, which translates to 4MB.
    The default size is 64MB. This will help in reducing
    kernel static by ~60MB.
    
    Change-Id: Id74ad0d92a5190720029ceb44585adf5ea579622

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bullhead boot_cpus=0-5
-BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 msm_poweroff.download_mode=0
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 msm_poweroff.download_mode=0 swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
