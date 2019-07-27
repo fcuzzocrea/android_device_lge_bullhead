d11340d3f29183c4a77b7cc01d304dc292c7b10d
Anay Wadhera <anay1018@gmail.com>
2019-07-27 16:44:13 -0700

    bullhead: use OSS libtinyxml
    Change-Id: I13a1273fb3615f858acd5d2f89bcc671e37003b7

--- device.mk
+++ device.mk
@@ -595,5 +595,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.facelock.rec_timeout=3500 \
     ro.facelock.est_max_time=600
 
+# general support
+PRODUCT_PACKAGES += \
+    libtinyxml
+
 $(call inherit-product-if-exists, hardware/qcom/msm8994/msm8992.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8994/msm8994-gpu-vendor.mk)
